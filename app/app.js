3638088f226ac7e35220f9763a343e755420a76a
iVan Vitali <webcoder.ivan@gmail.com>
2017-04-04 23:59:05 -0300

    Se creo la carpeta app que va a contener el codigo de aplicacion en AngularJS, se creo el modulo principal EncuestaRyvApp con el nombre del archivo app.js, se creo el archivo package.json que va a contener las dependencias del proyecto

---
+++ app/app.js
@@ -0,0 +1,14 @@
+(function)() {
+    'use strict';
+    
+    angular.module('EncuestaRyvApp', [
+        // Angular modules
+        'ngAnimate',
+        'ngRoute'
+        // Custom modules
+        
+        // 3rd Party Modules
+        
+    ]);
+    
+ })();
